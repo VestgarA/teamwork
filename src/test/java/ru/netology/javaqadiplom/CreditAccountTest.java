631cedce0aff80d0cf70aff3edd0c7b4144fdd57
vestgar <sid06@bk.ru>
2025-06-25 07:14:28 +0300

    errors in the tests

--- src/test/java/ru/netology/javaqadiplom/CreditAccountTest.java
+++ src/test/java/ru/netology/javaqadiplom/CreditAccountTest.java
@@ -17,4 +17,95 @@ public class CreditAccountTest {
 
         Assertions.assertEquals(3_000, account.getBalance());
     }
+
+    @Test
+    public void ReplenishmentNegativeAmount() {
+        CreditAccount account = new CreditAccount(
+                0,
+                5_000,
+                15
+        );
+
+        account.add(-1_000);
+
+        Assertions.assertEquals(0, account.getBalance());
+    }
+
+    @Test
+    public void ReplenishmentNull() {
+        CreditAccount account = new CreditAccount(
+                0,
+                5000,
+                15
+        );
+
+        account.add(0);
+
+        Assertions.assertEquals(0, account.getBalance());
+    }
+
+    @Test
+    public void purchaseWithinLimitsBalance() {
+        CreditAccount account = new CreditAccount(
+                4_000,
+                5_000,
+                15
+        );
+
+        account.pay(1_000);
+
+        Assertions.assertEquals(3_000, account.getBalance());
+    }
+
+    @Test
+    public void PurchaseAboveLimit() {
+        CreditAccount account = new CreditAccount(
+                0,
+                5000,
+                15
+        );
+
+        account.pay(6_000);
+
+        Assertions.assertEquals(0, account.getBalance());
+    }
+
+    @Test
+    public void purchaseWithinCreditLimit() {
+        CreditAccount account = new CreditAccount(
+                0,
+                5_000,
+                15
+        );
+        account.pay(1_000);
+
+        account.yearChange();
+        Assertions.assertEquals(-1_000, account.getBalance());
+    }
+
+    @Test
+    public void calculatingInterestNegativeBalance() {
+        CreditAccount account = new CreditAccount(
+                -200,
+                5_000,
+                15
+        );
+
+        account.yearChange();
+
+        Assertions.assertEquals(-30, account.getBalance());
+    }
+
+    @Test
+    public void calculatingInterestPositivBalance() {
+        CreditAccount account = new CreditAccount(
+                200,
+                5_000,
+                15
+        );
+
+        account.yearChange();
+
+        Assertions.assertEquals(0, account.getBalance());
+    }
 }
